e2523254aca3a7e274c2569ab71ecae21f2f912d
badr ghanbi <bghanbi50@gmail.com>
2022-12-11 16:31:17 +0100

    more refactoring

--- main.js
+++ main.js
@@ -12,35 +12,42 @@ closeOverlayIcon.addEventListener('click',()=>{
     overlay.classList.add('hidden')
 })
 
-
-form.addEventListener('submit',(e)=>{
+const createNote=(e)=>{
     e.preventDefault();
-   // create note
-    //get the enetered notes title and desc 
-    let noteItem={id:null,'note-title':'','note-desc':'','note-time':new Date().toDateString()}
-    form.querySelectorAll('input').forEach((input,index)=>{
-         noteItem[input.id]=input.value;
-         noteItem.id=index;
-    })
-    
+    // create note
+     form.querySelectorAll('input').forEach((input,index)=>{
+          noteItem[input.id]=input.value;
+          noteItem.id=index;
+     })
+     
+ 
+   // check if note object values are valid
+   if(Object.values(noteItem).some(value=>!value)){
+    alert('please enter valid string')
+    return
+   }
+ // push the created note to the notes array
+   notes.push(noteItem);
+ 
+ //   hide overlay
+ overlay.classList.add('hidden')
+}
 
-  // check if note object values are valid
-  if(Object.values(noteItem).some(value=>!value)){
-   alert('please enter valid string')
-   return
-  }
-// push the created note to the notes array
-  notes.push(noteItem);
-
-//   hide overlay
-overlay.classList.add('hidden')
-  // add the created note to notes
+const addNote=()=>{
+    // add the created note to notes
   let note=document.createElement('div');
   note.classList.add('card','note')
   note.innerHTML=`
      <h2 class='note__title'>${noteItem["note-title"]}</h2>
-     <p class='note__desc'>${noteItem["note-desc"]}<p>
-     <p class='note__time'>${noteItem['note-time']}<p>
+     <p class='note__desc'>${noteItem["note-desc"]}</p>
+     <p class='note__time'>${noteItem['note-time']}</p>
      `
     overlay.insertAdjacentElement('beforebegin',note)
+}
+
+form.addEventListener('submit',()=>{
+ let noteItem={id:null,'note-title':'','note-desc':'','note-time':new Date().toDateString()}
+  createNote();
+  addNote(noteItem);
+  
 })
