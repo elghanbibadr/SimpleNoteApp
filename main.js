0f5228eb64f4c9c2c18136b17b0aae2a2c349a67
badr ghanbi <bghanbi50@gmail.com>
2022-12-11 16:16:07 +0100

    more work on the note app

---
+++ main.js
@@ -0,0 +1,46 @@
+let form=document.querySelector('form');
+let addNoteIcon=document.querySelector('.addNoteIcon');
+let overlay=document.querySelector('.overlay');
+let closeOverlayIcon=document.querySelector('.closeIcon');
+let notes=[];
+addNoteIcon.addEventListener('click',()=>{
+    overlay.classList.remove('hidden');
+})
+
+
+closeOverlayIcon.addEventListener('click',()=>{
+    overlay.classList.add('hidden')
+})
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+   // create note
+    //get the enetered notes title and desc 
+    let noteItem={id:null,'note-title':'','note-desc':'','note-time':new Date().toDateString()}
+    form.querySelectorAll('input').forEach((input,index)=>{
+         noteItem[input.id]=input.value;
+         noteItem.id=index;
+    })
+    
+
+  // check if note object values are valid
+  if(Object.values(noteItem).some(value=>!value)){
+   alert('please enter valid string')
+   return
+  }
+// push the created note to the notes array
+  notes.push(noteItem);
+
+//   hide overlay
+overlay.classList.add('hidden')
+  // add the created note to notes
+  let note=document.createElement('div');
+  note.classList.add('card')
+  note.innerHTML=`
+     <h2>${noteItem["note-title"]}</h2>
+     <p>${noteItem["note-desc"]}<p>
+     <p>${noteItem['note-time']}<p>
+     `
+    overlay.insertAdjacentElement('beforebegin',note)
+})
